a7f861b95791d7860dd60c0fc127f0db86c36ecf
minazaher <menazaher115@gmail.com>
2023-11-25 21:30:18 +0200

    role selector addedd

---
+++ app/src/main/java/com/example/educationalgp/RoleSelectorActivity2.java
@@ -0,0 +1,14 @@
+package com.example.educationalgp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoleSelectorActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_role_selector2);
+    }
+}
