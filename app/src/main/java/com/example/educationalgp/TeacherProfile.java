3ecde06ad85e4b0865de5e00890650c194d5d0ca
minazaher <menazaher115@gmail.com>
2023-11-26 02:43:52 +0200

    teacher profile added

--- app/src/main/java/com/example/educationalgp/TeacherProfile.java
+++ app/src/main/java/com/example/educationalgp/TeacherProfile.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class RoleSelectorActivity2 extends AppCompatActivity {
+public class TeacherProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_role_selector2);
+        setContentView(R.layout.activity_teacher_profile);
     }
 }
